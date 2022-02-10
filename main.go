a87154fe1fcf9f4b5f20b0e194593b983c0b95b8
Tamal Saha <tamal@appscode.com>
2022-02-09 21:18:03 -0800

    added-all
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- main.go
+++ main.go
@@ -1,19 +1,3 @@
-/*
-Copyright AppsCode Inc. and Contributors
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
@@ -21,19 +5,19 @@ import (
 	"path/filepath"
 	"sort"
 
-	"kubepack.dev/kubepack/pkg/lib"
-	"kubepack.dev/lib-helm/pkg/action"
-	actionx "kubepack.dev/lib-helm/pkg/action"
-	"kubepack.dev/lib-helm/pkg/values"
-	chartsapi "kubepack.dev/preset/apis/charts/v1alpha1"
-
 	flag "github.com/spf13/pflag"
 	"gomodules.xyz/x/crypto/rand"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
 	"k8s.io/client-go/util/homedir"
+	"k8s.io/klog/v2"
 	clientcmdutil "kmodules.xyz/client-go/tools/clientcmd"
+	"kubepack.dev/kubepack/pkg/lib"
+	"kubepack.dev/lib-helm/pkg/action"
+	actionx "kubepack.dev/lib-helm/pkg/action"
+	"kubepack.dev/lib-helm/pkg/values"
+	chartsapi "kubepack.dev/preset/apis/charts/v1alpha1"
 )
 
 func main() {
